be84e754c58909892ca66bea5460f20fe05c6952
sou-M3 <YOUNG04@gmail.com>
2026-02-25 21:13:22 +0630

    name wrong worked new note

--- self-studyhub/Note.xaml.cs
+++ self-studyhub/Note.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,39 @@ namespace self_studyhub.Pages
         public NotePage()
         {
             InitializeComponent();
+        }
+        private void NewNote_Click(object sender, RoutedEventArgs e)
+        {
+            EditorOverlay.Visibility = Visibility.Visible;
+
+            var slideIn = new DoubleAnimation
+            {
+                From = 600,
+                To = 0,
+                Duration = TimeSpan.FromMilliseconds(300)
+            };
+
+            EditorTransform.BeginAnimation(TranslateTransform.XProperty, slideIn);
+
+            NoteTitleBox.Text = "Untitled Note";
+            NoteContentBox.Text = "";
+        }
+
+        private void CloseEditor_Click(object sender, RoutedEventArgs e)
+        {
+            var slideOut = new DoubleAnimation
+            {
+                From = 0,
+                To = 600,
+                Duration = TimeSpan.FromMilliseconds(300)
+            };
+
+            slideOut.Completed += (s, _) =>
+            {
+                EditorOverlay.Visibility = Visibility.Collapsed;
+            };
+
+            EditorTransform.BeginAnimation(TranslateTransform.XProperty, slideOut);
         }
     }
 }
