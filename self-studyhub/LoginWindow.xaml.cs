5ca16e755876f0e0a29950920d178c19493b80f2
sou-M3 <YOUNG04@gmail.com>
2026-03-01 21:40:41 +0630

    completed login

--- self-studyhub/LoginWindow.xaml.cs
+++ self-studyhub/LoginWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -47,24 +48,47 @@ namespace self_studyhub
 
         private void TogglePassword_Click(object sender, RoutedEventArgs e)
         {
-            if (!isVisible)
+            if (txtPassword.Visibility == Visibility.Visible)
             {
                 txtPasswordVisible.Text = txtPassword.Password;
+
                 txtPassword.Visibility = Visibility.Collapsed;
                 txtPasswordVisible.Visibility = Visibility.Visible;
 
+                txtPasswordVisible.Focus();
+                txtPasswordVisible.CaretIndex = txtPasswordVisible.Text.Length;
+
                 eyeIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.EyeOffOutline;
-                isVisible = true;
             }
             else
             {
                 txtPassword.Password = txtPasswordVisible.Text;
-                txtPassword.Visibility = Visibility.Visible;
+
                 txtPasswordVisible.Visibility = Visibility.Collapsed;
+                txtPassword.Visibility = Visibility.Visible;
+
+                txtPassword.Focus();
 
                 eyeIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.EyeOutline;
-                isVisible = false;
             }
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Fade in
+            DoubleAnimation fade = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(2));
+            loginPanel.BeginAnimation(OpacityProperty, fade);
+
+            // Slide up
+            DoubleAnimation slide = new DoubleAnimation(40, 0, TimeSpan.FromSeconds(1));
+            slide.EasingFunction = new QuadraticEase()
+            {
+                EasingMode = EasingMode.EaseOut
+            };
+
+            TranslateTransform transform = (TranslateTransform)loginPanel.RenderTransform;
+            transform.BeginAnimation(TranslateTransform.YProperty, slide);
         }
     }  
     }
