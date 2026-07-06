c8ddfb68f7df999179660ac041d8d9bf188535c6
Amphelisia <phoothazinmyint042@gmail.com>
2026-07-06 10:48:27 +0630

    task and goals database is ok

--- self-studyhub/Models/DatabaseHelper.cs
+++ self-studyhub/Models/DatabaseHelper.cs
@@ -9,10 +9,6 @@ namespace self_studyhub.Models
     public class DatabaseHelper
     {
         public static string ConnectionString =
-            @"Data Source=DESKTOP-8BBV09R\MSSQLSERVER01;
-            Initial Catalog=StudyControlDB;
-            Integrated Security=True;
-            Encrypt=True;
-            TrustServerCertificate=True";
+            @"Data Source=DESKTOP-19080AH\SQLEXPRESS;Initial Catalog=StudyControlDB;Integrated Security=True";
     }
 }
