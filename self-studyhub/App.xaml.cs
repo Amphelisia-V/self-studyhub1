e7291bbbce0c4062b5d45d2be288648cfcd80e93
Amphelisia <phoothazinmyint042@gmail.com>
2026-07-08 13:32:37 +0630

    all done

--- self-studyhub/App.xaml.cs
+++ self-studyhub/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Syncfusion.Licensing;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,11 @@ namespace self_studyhub
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JAaF5cX2pCfFN0Q35ZeENxcEVEYVZQRXxcQlZhSXxVdkJjXn1fcXVWRmVYUEx9XEY=");
+
+            InitializeComponent();
+        }
     }
 }
